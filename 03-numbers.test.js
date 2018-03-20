718821372c3f0847b4c42b7353cf340b0b557f0a
Stanislav Nikitin <nikitin.develop@gmail.com>
2018-03-20 14:24:00 +0300

    03 Numbers

--- 03-numbers.test.js
+++ 03-numbers.test.js
@@ -1,7 +1,19 @@
-test('2 + 3', (z) => {
+test('2 + 3', () => {
   const value = 2 + 3
 
-  expect(value).toEqual(5)
+  expect(value).toBeGreaterThan(4)
+  expect(value).toBeGreaterThanOrEqual(4.5)
+  expect(value).toBeLessThan(6.3)
+  expect(value).toBeLessThanOrEqual(6.3)
+
 })
 
 
+test('Add float numbers', () => {
+  const value = 0.1 + 0.2
+
+  // This won't work because of rounding error
+  // expect(value).toBe(0.3)
+  // This works
+  expect(value).toBeCloseTo(0.3)
+})
