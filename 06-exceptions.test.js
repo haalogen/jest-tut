286c08864313f29aacec5f88e25735e606e4b587
Stanislav Nikitin <nikitin.develop@gmail.com>
2018-03-20 14:44:52 +0300

    06 exceptions

---
+++ 06-exceptions.test.js
@@ -0,0 +1,11 @@
+function compileCode () {
+  throw new Error('cannot find compiler')
+}
+
+test('compiling goes as expected', () => {
+  expect(compileCode).toThrow()
+  expect(compileCode).toThrow(Error)
+
+  // You can also use the exact error message or a regexp
+  expect(compileCode).toThrow(/compiler/)
+})
