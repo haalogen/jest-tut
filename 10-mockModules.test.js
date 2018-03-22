441bb21cee2c8ca1e086a5fb3cecf1d37fbd3a44
Stanislav Nikitin <nikitin.develop@gmail.com>
2018-03-23 01:03:10 +0300

    10 Mock Modules

---
+++ 10-mockModules.test.js
@@ -0,0 +1,19 @@
+//
+// Mocking Modules
+//
+
+// Suppose we have a class that fetches users from our API using `axios`:
+import axios from 'axios'
+import {Users} from './Users'
+
+jest.mock('axios')
+
+it('should fetch users', () => {
+  const resp = {data: [{name: 'Bob'}]}
+
+  axios.get.mockResolvedValue(resp)
+  // or you could use the follwing depending on your use case:
+  // axios.get.mockImplementation(() => Promise.resolve(resp))
+
+  return Users.all().then(users => expect(users).toEqual(resp.data))
+})
